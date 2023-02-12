455acee0294c7d0587b1322d8d6daa1f27b06c15
Сергей Савчишкин <rings.of.saturn@yandex.ru>
2023-02-12 17:51:21 +0100

    add docstring

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from random import randint
 from graphic_arts.start_game_banner import run_screensaver
 
 def attack(char_name: str, char_class: str) -> str:
+    """Attack calculating."""
     if char_class == 'warrior':
         return (f"""{char_name} нанёс урон противнику
             равный {5 + randint(3, 5)}""")
@@ -15,6 +16,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Defence calculating."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -24,6 +26,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Class attack calculating."""
     if char_class == 'warrior':
         return (f"""{char_name} применил
             специальное умение «Выносливость {80 + 25}»""")
@@ -34,6 +37,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Command training."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -58,6 +62,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Class selector and introduction."""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
